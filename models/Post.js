9a7aa0e33b9c139b3fa1bca73b13b34bc410547d
curriecoder <andrew.j.currie0@gmail.com>
2021-10-19 16:41:49 -0400

    created associations in model index

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
-const { Model, DataTypes } = require('sequelizew');
-const sequelize = require('../config/connection');
+const { all } = require("bluebird");
+const { Model, DataTypes } = require("sequelizew");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
@@ -11,16 +12,34 @@ Post.init(
       primaryKey: true,
       autiIncrement: true,
     },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
     date: {
       type: DataTypes.DATE,
       allowNull: false,
-    }
+    },
+    user_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',  
+    modelName: "post",
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -3,3 +3,37 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 module.exports = { User, Post, Comment };
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete:'CASCADE',
+  hooks: true
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+  hooks: true
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+  hooks: true
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+  hooks: true
+});
+
+module.exports = { User, Post, Comment };
