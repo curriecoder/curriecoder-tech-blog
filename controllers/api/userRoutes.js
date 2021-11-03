0b6aeeb7afa2afe17516b971ca8d566dce2d4aee
curriecoder <andrew.j.currie0@gmail.com>
2021-11-03 10:42:47 -0400

    added login/logout routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -52,7 +52,7 @@ router.post("/login", async (req, res) => {
       req.session.email = dbUserData.email;
       req.session.loggedIn = true;
 
-      res.status(200).json({ message: "Logged in!"});
+      res.status(200).json({ message: "Logged in!" });
     });
   } catch (err) {
     res.status(500).json(err);
@@ -60,5 +60,35 @@ router.post("/login", async (req, res) => {
 });
 
 // POST new user
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST for logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
