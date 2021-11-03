4bf94e0a567f732b17eacc3859ad88ae57e46e3d
curriecoder <andrew.j.currie0@gmail.com>
2021-11-03 11:25:35 -0400

    continued building home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,25 @@
-const sequelize = require("../config/connection");
+const withAuth = require("../utils/auth");
 const { Post, User, Comment } = require("../models");
 const router = require("express").Router();
+
 router.get("/", (req, res) => {
-  Post.findAll({
-    attributes: ["id", "title", "content", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
           model: User,
           attributes: ["username"],
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      ],
     });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+  
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/login", (req, res) => {
