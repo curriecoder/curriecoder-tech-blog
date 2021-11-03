19fd05d365e48204b77d68af4d4d7a1943df1faf
curriecoder <andrew.j.currie0@gmail.com>
2021-11-03 13:38:39 -0400

    finished home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,6 +22,7 @@ router.get("/", (req, res) => {
   }
 });
 
+// GET login route
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
@@ -69,40 +70,50 @@ router.get("/post/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
-router.get("/posts-comments", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "content", "title", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      const post = dbPostData.get({ plain: true });
 
-      res.render("posts-comments", { post, loggedIn: req.session.loggedIn });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+// GET for post
+router.get("/post", (req, res) => {
+  if (!req.session.loggedIn) {
+    req.redirect("/login");
+    return;
+  }
+  res.render("post", { loggedIn: req.session.loggedIn });
 });
+
+// router.get("/posts-comments", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: ["id", "content", "title", "created_at"],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       const post = dbPostData.get({ plain: true });
+
+//       res.render("posts-comments", { post, loggedIn: req.session.loggedIn });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
