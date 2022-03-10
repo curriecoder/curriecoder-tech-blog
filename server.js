516dc7a0a72a30485c21cec10afb1116e4a09be7
curriecoder <andrew.j.currie0@gmail.com>
2022-03-10 16:18:20 -0500

    Reconfigured local .env, removed some commented lines from server.js

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers"));
-// const routes = require("./controllers");
-
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
